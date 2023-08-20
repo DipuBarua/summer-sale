1b899aaabab4f1a484d24bd750e4ab2b71906f97
DipuBarua <you@example.com>
2023-08-21 05:04:36 +0600

    updated all calculation

--- js/price-calculation.js
+++ js/price-calculation.js
@@ -4,7 +4,10 @@ function addToCalculationField(cardName) {
 
     const count = addCardField.childElementCount;
     const p = document.createElement('p');
-    p.innerHTML = `${count+1} ${cardName}`;
+    p.innerHTML = `${count+1}. ${cardName}`;
+    p.classList.add('mb-2');
+    p.classList.add('font-medium');
+
     addCardField.appendChild(p);
 }
 
@@ -13,23 +16,39 @@ function addTotalPrice(newPrice) {
     const totalPriceString = totalPrice.innerText;
     const totalPriceValue = parseFloat(totalPriceString);
     const newTotalPrice = totalPriceValue + newPrice;
-    totalPrice.innerText = newTotalPrice;
+    totalPrice.innerText = newTotalPrice.toFixed(2);
 
-    if (newTotalPrice >= 200) {
-        const discount = newTotalPrice * 0.2;
-        const totalDiscountField = document.getElementById('total-discount');
-        totalDiscountField.innerText = discount;
+    const makePurchaseBtn = document.getElementById('make-purchase-btn');
+    if (newTotalPrice > 0) {
+        makePurchaseBtn.removeAttribute('disabled');
+    } else {
+        makePurchaseBtn.setAttribute('disabled', true);
     }
 
+    const totalField = document.getElementById('total');
+    totalField.innerText = newTotalPrice;
+
+    const couponApplyBtn = document.getElementById('coupon-apply-btn');
+
+    if (newTotalPrice >= 200) {
+        couponApplyBtn.removeAttribute('disabled');
+
+        document.getElementById('coupon-code-field').addEventListener('keyup', function(event) {
+            const couponCodeText = event.target.value;
+
+            if (couponCodeText === 'SELL200') {
+                document.getElementById('coupon-apply-btn').addEventListener('click', function() {
+                    const discount = newTotalPrice * 0.2;
+                    const totalDiscountField = document.getElementById('total-discount');
+                    totalDiscountField.innerText = discount.toFixed(2);
+
+                    const newTotal = newTotalPrice - discount;
+                    totalField.innerText = newTotal.toFixed(2);
+                })
+            }
+        })
+
+    } else {
+        couponApplyBtn.setAttribute('disabled', true);
+    }
 }
-
-document.getElementById('first-card-field').addEventListener('click', function() {
-    addToCalculationField('K. Accessories');
-    addTotalPrice(22)
-})
-document.getElementById('second-card-field').addEventListener('click', function() {
-    addToCalculationField(' Accessories');
-
-    addTotalPrice(56);
-
-})
---
+++ js/utilities.js
@@ -0,0 +1,44 @@
+document.getElementById('first-card-field').addEventListener('click', function() {
+    addToCalculationField('K. Accessories');
+    addTotalPrice(39.00)
+})
+
+document.getElementById('second-card-field').addEventListener('click', function() {
+    addToCalculationField(' Accessories');
+    addTotalPrice(25.00);
+})
+
+document.getElementById('third-card-field').addEventListener('click', function() {
+    addToCalculationField(' Home Cooker');
+    addTotalPrice(49.00);
+})
+
+document.getElementById('first-card-2div-field').addEventListener('click', function() {
+    addToCalculationField(' Sports Back Cap');
+    addTotalPrice(49.00);
+})
+
+document.getElementById('second-card-2div-field').addEventListener('click', function() {
+    addToCalculationField(' Full Jersey Set');
+    addTotalPrice(69.00);
+})
+
+document.getElementById('third-card-2div-field').addEventListener('click', function() {
+    addToCalculationField(' Sports cates');
+    addTotalPrice(159.00);
+})
+
+document.getElementById('first-card-3div-field').addEventListener('click', function() {
+    addToCalculationField(' Single Relax Chair');
+    addTotalPrice(185.00);
+})
+
+document.getElementById('second-card-3div-field').addEventListener('click', function() {
+    addToCalculationField(' Children play');
+    addTotalPrice(299.00);
+})
+
+document.getElementById('third-card-3div-field').addEventListener('click', function() {
+    addToCalculationField(' Flexible Sofa');
+    addTotalPrice(339.00);
+})
